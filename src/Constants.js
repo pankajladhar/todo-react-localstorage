1319415c760bc1d1616fc4ce071bdf62f84b05ba
pankajladhar <ladharpankaj@gmail.com>
2017-01-27 19:43:13 +0530

    Removed unnessary files for gh-pages

--- src/Constants.js
+++
@@ -1,9 +0,0 @@
-const TODO_CONSTANTS={
-    INPUT_PLACEHOLDER_TEXT : "Enter text",
-    MESSAGE : "Bonjour, TODO List Example with React - ES6",
-    TODO_STORAGE : "todos"
-}
-
-module.exports={
-    TODO_CONSTANTS : TODO_CONSTANTS
-}
